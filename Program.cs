234a5853b8da2194e48d383bf5deb163a4e824e0
nordinzakaria <nordinzakaria@gmail.com>
2024-10-09 08:48:18 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+using System;
+using TransactionPackage;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Transaction transaction = new Transaction();
+        transaction.setVal(1);
+        transaction.setDate(DateTime.Now);
+
+        TransactionList transactionList = new TransactionList();
+        transactionList.Add(transaction);
+
+    }
+}
