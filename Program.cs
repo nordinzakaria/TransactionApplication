e5fe2c2f72683a5731fb343bd8cec190700016fe
nordinzakaria <nordinzakaria@gmail.com>
2024-10-09 09:55:02 +0800

    Firestore access works!

--- Program.cs
+++ Program.cs
@@ -8,11 +8,15 @@ class Program
     static void Main(string[] args)
     {
         Transaction transaction = new Transaction();
-        transaction.setVal(1);
-        transaction.setDate(DateTime.Now);
+        transaction.Val = 1;
+        transaction.Date = DateTime.Now;
+        transaction.Employee = new Employee();
 
         TransactionList transactionList = new TransactionList();
         transactionList.Add(transaction);
+
+        Business business = new Business();
+        business.initFirestore();
 
     }
 }
